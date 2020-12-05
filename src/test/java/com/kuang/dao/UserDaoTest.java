4de44d0002aec99e9279c277c8c7eee048a3909d
kalipy <3069087972@qq.com>
2020-12-05 22:16:29 +0800

    test 报错问题 Could not find resource com/kuang/dao/UserMapper.xml 请用tree build 看build目录下的resources目录

---
+++ src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,37 @@
+package com.kuang.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import org.junit.Test;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+
+/*
+ * UserDaoTest.java
+ * Copyright (C) 2020 2020-12-05 21:31 kalipy <kalipy@debian>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class UserDaoTest
+{
+    @Test
+	public void test() {
+	    SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一:getMapper
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭SqlSession
+        sqlSession.close();
+	}
+}
+

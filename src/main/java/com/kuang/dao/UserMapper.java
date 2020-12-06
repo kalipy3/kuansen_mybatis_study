37e8e5c9a26c00129b62652c783ec18b00b34d3b
kalipy <3069087972@qq.com>
2020-12-06 17:12:29 +0800

    注解开发 小问题：password字段没装配上 所以复杂的sql用xml

--- src/main/java/com/kuang/dao/UserMapper.java
+++ src/main/java/com/kuang/dao/UserMapper.java
@@ -3,6 +3,8 @@ package com.kuang.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.kuang.pojo.User;
 /*
  * UserDao.java
@@ -13,29 +15,7 @@ import com.kuang.pojo.User;
 
 public interface UserMapper
 {
-    //查询全部用户
-    List<User> getUserList();
-
-    //根据id查询用户
-    User getUserById(int id);
-
-    //insert一个用户
-    int addUser(User user);
-
-    //修改用户
-    int updateUser(User user);
-
-    //删除一个用户
-    int deleteUser(int id);
-
-    //万能的Map
-    int addUser2(Map<String, Object> map);
-
-    User getUserById2(Map<String, Object> map);
-
-    List<User> getUserLike(String value);
-
-    //分页
-    List<User> getUserByLimit(Map<String, Integer> map);
+    @Select("select * from user")
+    List<User> getUsers();
 }
 
--- src/test/java/com/kuang/dao/UserDaoTest.java
+++ src/test/java/com/kuang/dao/UserDaoTest.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Logger;
 
 import org.junit.Test;
 
+import com.kuang.dao.UserMapper;
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
 
@@ -25,39 +26,14 @@ public class UserDaoTest
     static Logger logger = Logger.getLogger(UserDaoTest.class);
 
     @Test
-    public void getUserById() {
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-
-        User user = mapper.getUserById(1);
-
-        System.out.println(user);
-        logger.info("user-------:"+user);
-
-        sqlSession.close();
-    }
-
-    @Test
-    public void testLog4j() {
-        logger.info("info:进入了testLog4j");
-        logger.debug("debug:进入了testLog4j");
-        logger.error("error:进入了testLog4j");
-    }
-
-    @Test
-    public void getUserByLimit() {
+    public void test() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put("startIndex", 0);
-        map.put("pageSize", 2);
-        List<User> userList = mapper.getUserByLimit(map);
-        for (User user : userList) {
+        List<User> users = mapper.getUsers();
+
+        for (User user : users) {
             logger.info("logger.info():"+user);
         }
-
         sqlSession.close();
     }
 }
